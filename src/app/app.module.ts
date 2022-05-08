ae84d02e0ce08578015533bb4ecc935175e4ac33
aymen-ben-massoud <aymenbm171998@gmail.com>
2022-05-08 19:34:39 +0200

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { SupportComponent } from './support/support.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent,
+    PricingComponent,
+    SupportComponent
   ],
   imports: [
+    ReactiveFormsModule, 
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
